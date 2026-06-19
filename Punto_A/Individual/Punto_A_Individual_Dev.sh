c841437ce84aeec90ba0df4f5825d9de77625198
vvalensosa <valentino.sosa24@gmail.com>
2026-06-18 21:56:08 -0300

    Corrijo punto individual del rol dev

--- Punto_A/Individual/Punto_A_Individual_Dev.sh
+++ Punto_A/Individual/Punto_A_Individual_Dev.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Punto A - Parte Individual - Rol: dev
+
 mkdir -p dev/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+
+# Crear un .gitkeep en cada carpeta hoja
 
 touch dev/Servicios/Web/.gitkeep \
 dev/Servicios/Base_de_Datos/.gitkeep \
@@ -8,3 +12,6 @@ dev/Servicios/Cache/.gitkeep \
 dev/Monitoreo/Logs/.gitkeep \
 dev/Monitoreo/Alertas/.gitkeep \
 dev/Monitoreo/Metricas/.gitkeep
+
+# Validación
+tree ~/dev/
