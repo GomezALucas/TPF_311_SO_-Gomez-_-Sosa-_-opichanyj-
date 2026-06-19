5c3e3009f68bebad8815958a32584f4ab90538cd
vvalensosa <valentino.sosa24@gmail.com>
2026-06-18 21:48:37 -0300

    Agrego punto individual del rol dev

---
+++ Punto_A/Individual/Punto_A_Individual_Dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p dev/{Servicios/{Web,Base_de_Datos,Cache},Monitoreo/{Logs,Alertas,Metricas}}
+
+touch dev/Servicios/Web/.gitkeep \
+dev/Servicios/Base_de_Datos/.gitkeep \
+dev/Servicios/Cache/.gitkeep \
+dev/Monitoreo/Logs/.gitkeep \
+dev/Monitoreo/Alertas/.gitkeep \
+dev/Monitoreo/Metricas/.gitkeep
