4367af6d2c2b055ce01996e475d88b7fec69b65e
vvalensosa <valentino.sosa24@gmail.com>
2026-06-18 22:01:01 -0300

    Corrijo bug de la parte del dev

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -34,7 +34,7 @@ touch /Continentes/Europa/Francia/Paris/.gitkeep
 # --- PARTE DEV - Continente: Asia ---
 # Estructura: 3 países con distribución asimétrica 3-2-1
 
-mkdir -p /Continentes/Asia/{Japon/{Tokio,Osaka,Kioto},China/{Pekin,Shanghai},CoreaDelSur/{Seul}}
+mkdir -p /Continentes/Asia/{Japon/{Tokio,Osaka,Kioto},China/{Pekin,Shanghai},CoreaDelSur/Seul}
 
 touch /Continentes/Asia/Japon/Tokio/.gitkeep
 touch /Continentes/Asia/Japon/Osaka/.gitkeep
