53a525f851bb658eeeb0b0817e4be60c6b103d0a
vvalensosa <valentino.sosa24@gmail.com>
2026-06-26 03:17:43 -0300

    Agrego parte DEV al Punto C

--- Punto_C/Punto_C.sh
+++ Punto_C/Punto_C.sh
@@ -26,3 +26,42 @@ id u_dev      >> /home/vagrant/carpeta_compartida/Punto_C/id_output.txt
 
 echo "id_output.txt guardado:"
 cat /home/vagrant/carpeta_compartida/Punto_C/id_output.txt
+
+# ==========================================
+
+#Punto C: De rol DEV.
+
+# Paso 1:
+# Crear los grupos principales.
+
+groupadd -f g_sysadmin
+groupadd -f g_manager
+groupadd -f g_dev
+
+# Paso 2:
+# Crear el grupo secundario.
+
+groupadd -f g_infra
+
+# Paso 3:
+# Generar el hash de las contraseñas.
+
+PASS_SYSADMIN=$(openssl passwd -6 "u_sysadmin")
+PASS_MANAGER=$(openssl passwd -6 "u_manager")
+PASS_DEV=$(openssl passwd -6 "u_dev")
+
+# Paso 4:
+# Crear los usuarios.
+
+useradd -m -g g_sysadmin -G g_infra -s /bin/bash -p "$PASS_SYSADMIN" u_sysadmin
+
+useradd -m -g g_manager -G g_infra -s /bin/bash -p "$PASS_MANAGER" u_manager
+
+useradd -m -g g_dev -G g_infra -s /bin/bash -p "$PASS_DEV" u_dev
+
+# Paso 5:
+# Mostrar la información de cada usuario.
+
+id u_sysadmin > /home/vagrant/carpeta_compartida/Punto_C/id_output.txt
+id u_manager >> /home/vagrant/carpeta_compartida/Punto_C/id_output.txt
+id u_dev >> /home/vagrant/carpeta_compartida/Punto_C/id_output.txt
