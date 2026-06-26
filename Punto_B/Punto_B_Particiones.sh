a60062557ffb5ad25a7a66a14675b1fdbfb2dfc4
vvalensosa <valentino.sosa24@gmail.com>
2026-06-26 00:32:10 -0300

    Agrego las particiones del rol dev

--- Punto_B/Punto_B_Particiones.sh
+++ Punto_B/Punto_B_Particiones.sh
@@ -54,3 +54,80 @@ mkfs.ext4 -F /dev/sdc7
 
 echo "Particionamiento y formateo completados."
 lsblk /dev/sdc
+
+
+#Particiones del rol dev:
+#estructura:
+#sdc1 -> primaria 1GB
+#sdc2 -> primaria 1GB
+#sdc3 -> extendida 8GB (resto ~8 GB)
+#sdc4 -> logica ~2.6 GB
+#sdc5 -> logica ~2.6 GB
+#sdc6 -> logica ~2.6 GB
+
+
+fdisk /dev/sdc << EOF
+
+o
+n
+p
+1
+
++1G
+n
+p
+2
+
++1G
+n
+e
+3
+
+
+n
+
++2600M
+n
+
++2600M
+n
+
+
+w
+EOF
+
+#el kernel registre las nuevas particiones
+partprobe /dev/sdc
+sleep 2
+
+
+mkfs.ext4 -F /dev/sdc1
+mkfs.ext4 -F /dev/sdc2
+mkfs.ext4 -F /dev/sdc5
+mkfs.ext4 -F /dev/sdc6
+mkfs.ext4 -F /dev/sdc7
+
+echo "Particiones creadas y formateo correctamente."
+
+lsblk /dev/sdc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
